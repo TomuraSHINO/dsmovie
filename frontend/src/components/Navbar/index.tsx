dbb92fc92d97d7e63bb00a93966eded837c41216
Daniel R. Xavier <danielxavier2077@gmail.com>
2022-03-20 13:40:39 -0300

    Form layout

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,7 +1,7 @@
 import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
 import './styles.css';
 
-const Navbar = () => {
+function Navbar() {
     return (
         <header>
             <nav className="container">
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+function Listing() {
+    return (
+        <h1>d</h1>
+    );
+}
+
+export default Listing;
